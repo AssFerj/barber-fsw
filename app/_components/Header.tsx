78bca7e9ff1f177d920f39ed1b4cefe363bb15a7
AssFerj <assisjuniorcam@gmail.com>
2024-02-04 15:10:49 -0300

    feat: add initial header component

---
+++ app/_components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card, CardContent } from './ui/card';
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { MenuIcon } from 'lucide-react';
+
+const Header: React.FC = () => {
+  return (
+    <React.Fragment>
+        <Card>
+            <CardContent className='p-5 flex justify-around items-center'>
+                <Image src="/logo.png" alt="FSW Barber" width={120} height={22} />
+                <Button variant={'outline'} size={'icon'} className='h-8 w-8'>
+                    <MenuIcon size={16}/>
+                </Button>
+            </CardContent>
+        </Card>
+    </React.Fragment>
+  );
+};
+
+export default Header;
